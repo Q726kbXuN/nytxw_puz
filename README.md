ad38e97f2481f5ce00e27a98240c0e2dc6013e32
Random Coder <random.coder@gmail.com>
2021-08-05 15:08:39 -0700

    Make the readme a little prettier

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # NYT Crossword to Puz
 
-Convert NY Times crosswords to Across Lite puzzles.
+![Logo](other/logo.png)
+
+A windows program to convert NY Times crosswords from the web to Across Lite compatible files.
 
 To run this, first download and decompress the [release](https://github.com/Q726kbXuN/nytxw_puz/releases/latest/download/nytxw_puz.zip).  Then, run the executable, and answer the two questions:
 
@@ -30,3 +32,18 @@ Then, to use:
 ```
 nyt.py https://www.nytimes.com/crosswords/game/daily/2021/08/03 2021-08-03.puz
 ```
+
+# Mini FAQ
+
+* What? Why?
+
+The New York Times announced that on August 10th, [they will no longer provide](https://www.nytimes.com/2021/08/02/crosswords/nyt-games-no-longer-available-on-across-lite-as-of-aug-9.html) Across Lite .puz files for download.  This tool was made in response to that decision, allowing users of that tool to continue to solve crossword puzzles in their favorite program.
+
+* Why not just use the NY Times app?
+
+For me personally, I want something that works completely offline, and doesn't require I stare at my phone even more.  I'm sure other people have other reasons, so I decided to put my little tool online for others to use.
+
+* Anything else?
+
+Feel free to contact me on [reddit](https://www.reddit.com/user/nobody514/).
+
---
+++ other/logo_puzzle.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir) 
+import nyt
+
+puzzle_data = """
+. . . x x x x a x x x . . . x x x
+n y t i m e s c r o s s w o r d s
+x x x x x x x r . . x x e x x x .
+x x x x x x x o . x x x b x x x .
+x x . . . . x s . x x x p x x x .
+x x . x x x x s x x x x a x x x x
+x x . x x x x l x x x x g x . x x
+x x x x x x x i x x x x e x . x x
+. c o n v e r t . x x . . . . x x
+. x x x x x x e . x x x x x x x x
+. x x x x x x . . x x x x x x x x
+x x x . . . x x x x x x x x . . .
+"""
+
+def add_clue(x, y, puzzle, x_dir, y_dir):
+    clue = ""
+    while x < len(puzzle_data[0]) and y < len(puzzle_data) and puzzle_data[y][x] != ".":
+        clue += puzzle_data[y][x]
+        x += x_dir
+        y += y_dir
+    if "x" in clue:
+        clue = "-"
+    puzzle['clues'].append({"text": clue.upper()})
+
+
+def main():
+    global puzzle_data
+
+    puzzle = {
+        "dimensions": {
+            "rowCount": 0,
+            "columnCount": 0,
+        },
+        "cells": [],
+        "clues": [
+        ],
+        "meta": {
+            "constructors": [],
+        },
+    }
+    output_fn = "logo_puzzle.puz"
+
+    puzzle_data = [x.strip("\r\n").split(" ") for x in puzzle_data.split("\n") if len(x.strip("\r\n"))]
+
+    for y in range(len(puzzle_data)):
+            for x in range(len(puzzle_data[0])):
+                if puzzle_data[y][x] != ".":
+                    if x < len(puzzle_data[0]) - 1:
+                        if x == 0:
+                            if puzzle_data[y][x + 1] != ".":
+                                add_clue(x, y, puzzle, 1, 0)
+                        else:
+                            if puzzle_data[y][x-1] == "." and puzzle_data[y][x+1] != ".":
+                                add_clue(x, y, puzzle, 1, 0)
+                    if y < len(puzzle_data) - 1:
+                        if y == 0:
+                            if puzzle_data[y + 1][x] != ".":
+                                add_clue(x, y, puzzle, 0, 1)
+                        else:
+                            if puzzle_data[y-1][x] == "." and puzzle_data[y+1][x] != ".":
+                                add_clue(x, y, puzzle, 0, 1)
+
+    puzzle['dimensions']['columnCount'] = len(puzzle_data[0])
+    puzzle['dimensions']['rowCount'] = len(puzzle_data)
+
+    for row in puzzle_data:
+        for cell in row:
+            if cell == ".":
+                puzzle['cells'].append({})
+            else:
+                puzzle['cells'].append({'answer': cell.upper()})
+    puzzle['gamePageData'] = puzzle
+
+    nyt.print_puzzle(puzzle)
+    output = nyt.data_to_puz(puzzle)
+    output.save(output_fn)
+    print(f"Created {output_fn}")
+
+
+if __name__ == "__main__":
+    main()
