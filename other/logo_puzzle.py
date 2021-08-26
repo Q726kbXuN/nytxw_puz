87ec2c8f2772fc996f6aab62b1d15b6415551f2d
Random Coder <random.coder@gmail.com>
2021-08-26 14:00:50 -0700

    Correct clue layout

--- other/logo_puzzle.py
+++ other/logo_puzzle.py
@@ -24,6 +24,7 @@ x x x . . . x x x x x x x x . . .
 """
 
 def add_clue(x, y, puzzle, x_dir, y_dir):
+    puzzle["temp"][(x, y, x_dir, y_dir)] = len(puzzle['clues'])
     clue = ""
     while x < len(puzzle_data[0]) and y < len(puzzle_data) and puzzle_data[y][x] != ".":
         clue += puzzle_data[y][x]
@@ -48,38 +49,44 @@ def main():
         "meta": {
             "constructors": [],
         },
+        "temp": {},
     }
     output_fn = "logo_puzzle.puz"
 
     puzzle_data = [x.strip("\r\n").split(" ") for x in puzzle_data.split("\n") if len(x.strip("\r\n"))]
 
     for y in range(len(puzzle_data)):
-            for x in range(len(puzzle_data[0])):
-                if puzzle_data[y][x] != ".":
-                    if x < len(puzzle_data[0]) - 1:
-                        if x == 0:
-                            if puzzle_data[y][x + 1] != ".":
-                                add_clue(x, y, puzzle, 1, 0)
-                        else:
-                            if puzzle_data[y][x-1] == "." and puzzle_data[y][x+1] != ".":
-                                add_clue(x, y, puzzle, 1, 0)
-                    if y < len(puzzle_data) - 1:
-                        if y == 0:
-                            if puzzle_data[y + 1][x] != ".":
-                                add_clue(x, y, puzzle, 0, 1)
-                        else:
-                            if puzzle_data[y-1][x] == "." and puzzle_data[y+1][x] != ".":
-                                add_clue(x, y, puzzle, 0, 1)
+        for x in range(len(puzzle_data[0])):
+            if puzzle_data[y][x] != ".":
+                if x < len(puzzle_data[0]) - 1:
+                    if x == 0:
+                        if puzzle_data[y][x + 1] != ".":
+                            add_clue(x, y, puzzle, 1, 0)
+                    else:
+                        if puzzle_data[y][x-1] == "." and puzzle_data[y][x+1] != ".":
+                            add_clue(x, y, puzzle, 1, 0)
+                if y < len(puzzle_data) - 1:
+                    if y == 0:
+                        if puzzle_data[y + 1][x] != ".":
+                            add_clue(x, y, puzzle, 0, 1)
+                    else:
+                        if puzzle_data[y-1][x] == "." and puzzle_data[y+1][x] != ".":
+                            add_clue(x, y, puzzle, 0, 1)
 
     puzzle['dimensions']['columnCount'] = len(puzzle_data[0])
     puzzle['dimensions']['rowCount'] = len(puzzle_data)
 
-    for row in puzzle_data:
-        for cell in row:
+    for y, row in enumerate(puzzle_data):
+        for x, cell in enumerate(row):
             if cell == ".":
-                puzzle['cells'].append({})
+                puzzle['cells'].append({'clues': []})
             else:
-                puzzle['cells'].append({'answer': cell.upper()})
+                puzzle['cells'].append({'answer': cell.upper(), 'clues': []})
+            if (x, y, 1, 0) in puzzle['temp']:
+                puzzle['cells'][-1]['clues'].append(puzzle['temp'][(x, y, 1, 0)])
+            if (x, y, 0, 1) in puzzle['temp']:
+                puzzle['cells'][-1]['clues'].append(puzzle['temp'][(x, y, 0, 1)])
+
     puzzle['gamePageData'] = puzzle
 
     nyt.print_puzzle(puzzle)
