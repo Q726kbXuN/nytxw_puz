9d32a3a8d3e205b0156f3eda13c3626a2237cb47
Random Coder <random.coder@gmail.com>
2022-05-01 09:50:10 -0700

    Update version to 1.09

--- version.py
+++ version.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # This next line must be exactly this format
-VERSION = "1.08"
+VERSION = "1.09"
 # The location of this file on GitHub
 SELF_URL = "https://raw.githubusercontent.com/Q726kbXuN/nytxw_puz/master/version.py"
 
